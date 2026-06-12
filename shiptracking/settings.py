ba14e178f1703e1b63940de5ae8e259950e1cff1
Ubuntu <ubuntu@ip-172-31-38-45.ap-south-1.compute.internal>
2026-06-12 05:12:44 +0000

    correct static

--- shiptracking/settings.py
+++ shiptracking/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
