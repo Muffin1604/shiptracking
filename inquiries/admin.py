931401b4fae516c0cc1cc95c3e8f2b701498b2a1
Karan saini <karansaini@Karans-MacBook-Air.local>
2026-07-23 17:04:52 +0530

    feedback form

--- inquiries/admin.py
+++ inquiries/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ContactInquiry
+from .models import ContactInquiry, FeedbackInquiry
 
 admin.site.register(ContactInquiry)
+admin.site.register(FeedbackInquiry)
--- inquiries/views.py
+++ inquiries/views.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ContactInquirySerializer
+from .serializers import ContactInquirySerializer, FeedbackInquirySerializer
 # Create your views here.
 class ContactInquiryView(APIView):
     permission_classes = [AllowAny]
@@ -18,6 +18,25 @@ class ContactInquiryView(APIView):
             return Response(response, status=status.HTTP_201_CREATED)
         response = {
             "message": "Inquiry failed",
+            "status": "error",
+            "errors": serializer.errors
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+class FeedbackInquiryView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = FeedbackInquirySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message": "Feedback sent successfully",
+                "status": "success",
+                "data": serializer.data
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
+        response = {
+            "message": "Feedback failed",
             "status": "error",
             "errors": serializer.errors
         }
